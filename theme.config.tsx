9b4d0587f879e06ea82271105b97973272ed710d
thisisouvik <souvikmandals10@gmail.com>
2026-08-26 02:54:05 +0530

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span style={{ fontWeight: 800, fontSize: "1.2rem", letterSpacing: "-0.5px" }}>Kredex Docs</span>,
+  project: {
+    link: 'https://github.com/thisisouvik/kredex',
+  },
+  docsRepositoryBase: 'https://github.com/thisisouvik/kredex',
+  footer: {
+    text: '© 2026 Kredex - Decentralized Micro-Lending on Stellar',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Kredex'
+    }
+  }
+}
+
+export default config
